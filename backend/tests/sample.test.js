401ff50572ad4cd08c68b0541a0de9611b084eef
Mishti Sawhney <mishtisawhney@gmail.com>
2026-04-28 21:35:30 +0530

    add supertest dependency

--- backend/tests/sample.test.js
+++ backend/tests/sample.test.js
@@ -4,3 +4,12 @@ test('basic addition works', () => {
 
 
   
+  const request = require('supertest');
+  const app = require('../app');
+  
+  describe('API Test', () => {
+    it('GET / should return 200', async () => {
+      const res = await request(app).get('/');
+      expect(res.statusCode).toBe(200);
+    });
+  });

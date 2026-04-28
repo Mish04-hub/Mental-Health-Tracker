820def0d9d251a88700122c1fd6e837b2192fdfb
Mishti Sawhney <mishtisawhney@gmail.com>
2026-04-28 21:33:53 +0530

    final sonar integration

--- backend/tests/sample.test.js
+++ backend/tests/sample.test.js
@@ -1,6 +1,9 @@
-test('basic addition works', () => {
-    expect(2 + 2).toBe(4);
+const request = require('supertest');
+const app = require('../app');
+
+describe('API Test', () => {
+  it('GET / should return 200', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
   });
-
-
-  
+});
